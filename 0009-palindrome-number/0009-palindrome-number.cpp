cd2097ffddf86f2ec264e122371ecb0bc17f9702
DEEPIKA K <deepika28226@gmail.com>
2026-03-10 21:02:35 +0530

    Time: 12 ms (5.99%), Space: 8.5 MB (65.36%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,32 @@
+#include<climits>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int initial=x;
+        int res=0;
+
+        if(x<0)
+            return false;
+
+        while(x!=0)
+        {
+            int n=x%10;
+            if(res < INT_MIN/10 || res > INT_MAX/10)
+            {
+                return false;
+            }
+
+            res=res*10+n;
+            x=x/10;
+        }
+
+        if(res==initial)
+        {
+            return true;
+        }
+
+        else
+            return false;
+    }
+};
