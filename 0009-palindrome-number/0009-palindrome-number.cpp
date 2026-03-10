e68eab0f338b06b04ee2ae6d09555cc0ae6f7180
DEEPIKA K <deepika28226@gmail.com>
2026-03-10 21:06:32 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.63%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -12,10 +12,12 @@ public:
         while(x!=0)
         {
             int n=x%10;
+            
             if(res < INT_MIN/10 || res > INT_MAX/10)
             {
                 return false;
             }
+            
 
             res=res*10+n;
             x=x/10;
