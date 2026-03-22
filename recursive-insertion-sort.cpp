cf7c2e6cf1abcf99ece199e0bbe4130a009e846c
Deepikachettiar <deepika28226@gmail.com>
2026-03-22 21:22:15 +0530

    recursive insertion sort

--- recursive-insertion-sort.cpp
+++ recursive-insertion-sort.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> insertionSort(vector<int>& nums) {
         int n=nums.size();
-        int i=0;
+        int i=1;
         while(i<n)
         {
-            sort(nums,nums[i]);
+            sort(nums,i);
             ++i;
         }
         return nums;
     }
 
-    void sort(vector<int>& nums,int key)
-    {
-        int j=nums.size();
+    void sort(vector<int>& nums,int i)
+    {   int key=nums[i];
+        int j=i;
         while(j>0 && nums[j-1]>key )
         {
             nums[j]=nums[j-1];
