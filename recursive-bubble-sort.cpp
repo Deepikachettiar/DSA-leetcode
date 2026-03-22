91401ac749d638b284586bb3fbce36817596b63c
Deepikachettiar <deepika28226@gmail.com>
2026-03-22 19:57:52 +0530

    recursive bubble sort

---
+++ recursive-bubble-sort.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> bubbleSort(vector<int>& nums) {
+        vector<int> sorted=sort(nums,nums.size());
+        return sorted;   
+    }
+
+    vector<int> sort(vector<int> &nums,int n)
+    {
+        if(n==1)
+        {
+            return nums;
+        }
+        for(int i=0;i<n-1;i++)
+        {
+            if(nums[i] > nums[i+1])
+            {
+                swap(nums[i],nums[i+1]);
+            }
+        }
+
+        sort(nums,n-1);
+    }
+};
