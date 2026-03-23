a0d9611c4a3db1851bcc67e7764dd3b23955d796
Deepikachettiar <deepika28226@gmail.com>
2026-03-23 22:12:05 +0530

    quick sort

---
+++ quick-sort.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> quickSort(vector<int>& nums) {
+        sort(nums,0,nums.size());
+        return nums;
+    }
+
+    void sort(vector<int> &nums,int l,int r)
+    {
+        if (l>=r)
+            return;
+
+        int i=l;
+        int j=r+1;
+        int p=nums[l];
+
+        while (i<j)
+        {
+            while(nums[i]<p)
+            {
+                ++i;
+            }
+            while(nums[j]>p)
+            {
+                ++j;
+            }
+            swap(nums[i],nums[j]);
+        }
+
+        swap(nums[i],nums[j]);
+        swap(nums[l],nums[j]);
+        sort(nums,)
+    }
+};
