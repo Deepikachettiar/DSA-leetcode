10f3d2a4d0fc3dc4df69285d1778a7998e85be5e
Deepikachettiar <deepika28226@gmail.com>
2026-03-24 22:12:06 +0530

    quick sort

---
+++ largest-element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestElement(vector<int>& nums) {
+        int maxi = nums[0];  // assume first element is largest
+
+        for(int i = 1; i < nums.size(); i++)
+        {
+            if(nums[i] > maxi)
+            {
+                maxi = nums[i];  // update if found larger
+            }
