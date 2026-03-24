f8cbaf377e6151ed6c2afcc67fafa165a6b0505b
Deepikachettiar <deepika28226@gmail.com>
2026-03-24 22:14:36 +0530

    largest element

--- largest-element.cpp
+++ largest-element.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int largestElement(vector<int>& nums) {
-        int maxi = nums[0];  // assume first element is largest
+        int maxi = nums[0]; 
 
         for(int i = 1; i < nums.size(); i++)
         {
             if(nums[i] > maxi)
             {
-                maxi = nums[i];  // update if found larger
+                maxi = nums[i]; 
             }
         }
 
