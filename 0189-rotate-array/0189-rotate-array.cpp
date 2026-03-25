4d7e2888bd2bd74c61f67f4815b9f8327bb36ef8
DEEPIKA K <deepika28226@gmail.com>
2026-03-25 23:17:14 +0530

    Time: 0 ms (100%), Space: 29.6 MB (66.99%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin()+k,nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+    }
+};
+
+
+/*
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        while(k>0)
+        {
+            int val=nums[nums.size()-1];
+            for(int i=nums.size()-1 ; i>0 ; i--)
+            {
+                nums[i]=nums[i-1];
+            }
+            nums[0]=val;
+            --k;
+        }
+    }
+};
+*/
