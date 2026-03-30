ddba34cb12435a7decc9db29bc756f243fb5a946
Deepikachettiar <deepika28226@gmail.com>
2026-03-30 23:13:20 +0530

    union of 2 arrays

---
+++ union-of-2-arrays.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
+     set<int> s;
+     int i=0;
+     int j=0;
+     while( i<nums1.size() && j<nums2.size() )   
+     {
+        if(nums1[i]<nums2[j])
+        {
+            s.insert(nums1[i]);
+            i++;
+        }
+
+        else
+        {
+            s.insert(nums2[j]);
+            j++;
+        }
+     }
+     while(i<nums1.size())
+     {
+        s.insert(nums1[i]);
+        i++;
+     }
+
+     while(j<nums2.size())
+     {
+        s.insert(nums2[j]);
+        j++;
+     }
+
+     return s;
+    }
+};
