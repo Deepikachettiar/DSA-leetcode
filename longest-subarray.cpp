d1b5ab5aef90d54cb6c5dfac67a7735bcf2505bc
Deepikachettiar <deepika28226@gmail.com>
2026-04-06 21:43:38 +0530

    longest subarray

--- longest-subarray.cpp
+++ longest-subarray.cpp
@@ -2,14 +2,22 @@ class Solution{
 public:
     int longestSubarray(vector<int> &nums, int k){
         int maxi=0;
+        int len=0;
 
         for (int i=0;i<nums.size();i++)
         {
-            
-            long long sum=0;
-            sum += nums[i];
+            int j=i;
+            while(j < nums.size())
+            {
+                long long sum=0;
+                sum += nums[i];
 
-            if(sum==k);
+                if(sum==k)
+                    len=j-i;
+                    maxi=max(len,maxi);
+            }
         }
+
+        return maxi;
     }
 };
