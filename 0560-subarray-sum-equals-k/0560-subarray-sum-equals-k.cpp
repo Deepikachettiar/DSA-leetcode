2a3ec616fe0a30b2537d13c50f8c153f0c28d6d9
DEEPIKA K <deepika28226@gmail.com>
2026-06-06 23:11:20 +0530

    40 ms (64.83%)

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) 
+    {
+
+        int n=nums.size();
+
+        //using hash map
+        //this hash mao stores the 
+        unordered_map<int,int> setsum;
+
+        int count=0;
+        int presum=0;
+
+        setsum[0]=1;
+
+        for(int i=0;i<n;i++)
+        {
+            presum=presum+nums[i];
+            int rem=presum-k;
+
+            if(setsum.find(rem)!=setsum.end())
+            {
+                count+=setsum[rem];
+            }
+
+            setsum[presum]++;
+        }
+
+        return count;
+    }
+};
