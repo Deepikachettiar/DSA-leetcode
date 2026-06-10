6ed7c9e93a411deb6864435ce8050351a1beee6f
DEEPIKA K <deepika28226@gmail.com>
2026-06-10 23:39:40 +0530

    8 ms (29.25%)

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         int n=nums.size();
         vector <int> arr;
-        unordered_map<int,int> x;
+        unordered_map<int,int> x;  //using hash map here
 
         for(int a : nums)
         {
             x[a]++;
         }
 
-        for(auto a : x)
+        for(auto a : x)  // as x is a hash mapped fucntion when we have to usein for lop we give auto data type
         {
             if(a.second>n/3)
             {
