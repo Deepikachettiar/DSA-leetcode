7ff06ea0d2d433f86a97d48bf5d34b4ba564de57
DEEPIKA K <deepika28226@gmail.com>
2026-06-11 22:49:23 +0530

    23 ms (54.73%)

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,47 +2,50 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> a;
+        int n=nums.size();
+
+        if(n<4)
+            return a;
+
         sort(nums.begin(),nums.end());
 
-        if(nums.size()<4)
-            return {};
-
-        for(int i=0;i<nums.size()-3;i++)
+        for(int i=0;i<n-3;i++)
         {
             if(i>0 && nums[i]==nums[i-1])
-            {
                 continue;
-            }
-            for(int j=i+1;j<nums.size()-2;j++)
+
+            for(int j=i+1;j<n-2;j++)
             {
                 if(j>i+1 && nums[j]==nums[j-1])
                     continue;
-                
+
                 int left=j+1;
-                int right=nums.size()-1;
+                int right=n-1;
 
                 while(left<right)
                 {
-                    if((long long)nums[i]+nums[j]+nums[left]+nums[right]==target)
+                    long long sum=(long long)nums[i]+nums[j]+nums[left]+nums[right];
+
+                    if(sum==target)
                     {
                         a.push_back({nums[i],nums[j],nums[left],nums[right]});
+
                         left++;
                         right--;
 
-                        while(left<right && nums[right]==nums[right+1])
-                            right--;
-                        
                         while(left<right && nums[left]==nums[left-1])
                             left++;
 
+                        while(left<right && nums[right]==nums[right+1])
+                            right--;
                     }
 
-                    else if((long long)nums[i]+nums[j]+nums[left]+nums[right]<target)
+                    else if(sum<target)
                     {
                         left++;
                     }
 
-                    else if((long long)nums[i]+nums[j]+nums[left]+nums[right]>target)
+                    else
                     {
                         right--;
                     }
