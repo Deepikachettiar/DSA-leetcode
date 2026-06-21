55dbb223f160b769c6a7eaf1a0e97e7c0461e03a
DEEPIKA K <deepika28226@gmail.com>
2026-06-21 20:23:54 +0530

    Update Count-subarrays-with-given-xor-K.cpp

--- Count subarrays with given xor K/Count-subarrays-with-given-xor-K.cpp
+++ Count subarrays with given xor K/Count-subarrays-with-given-xor-K.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int subarraysWithXorK(vector<int>& A, int k) {
-        unordered_map<int,int> arr;
+        unordered_map<int,int> arr;//we use this as a stack to add the history of the previous xor
         arr[0]=1;
         int count=0;
         int x=0;
 
-        for(int i=0;i<A.size();i++)
+        for(int i=0;i<A.size();i++) 
         {
             int a=A[i];
-            x=x^a;
+            x=x^a; //current value of xor witht the present element
 
-            int seek=x^k;
+            int seek=x^k; //the value which shd be there in the subarray to get k as the xor value
 
             if(arr.find(seek)!=arr.end())
             {
