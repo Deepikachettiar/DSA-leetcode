95a9e71bc0df639bea3819f97d4345987b66b3fc
DEEPIKA K <deepika28226@gmail.com>
2026-06-21 18:48:51 +0530

    Improve logic for finding repeating and missing numbers
    Refactor calculations for missing and repeating numbers.

--- Find the repeating and missing numbers/Find-the-repeating-and-missing-number.cpp
+++ Find the repeating and missing numbers/Find-the-repeating-and-missing-number.cpp
@@ -6,10 +6,11 @@ public:
 
         long long SN = (n * (n + 1)) / 2;
         
-
+        // Sum of squares of first n natural numbers
         long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
 
-
+        /*Calculate actual sum (S) and sum 
+        of squares (S2) of array elements*/
         long long S = 0, S2 = 0;
         for (int i = 0; i < n; i++) {
             S += nums[i];
@@ -17,15 +18,20 @@ public:
         }
 
 
-        long long val1 = S - SN; 
+        long long val1 = S - SN; //X-Y
         
-        long long val2 = S2 - S2N; 
+        long long val2 = S2 - S2N; //X^2-Y^2
 
-        val2 = val2 / val1;
+        //Calculate X + Y using X + Y = (X^2 - Y^2) / (X - Y)
+        val2 = val2 / val1; //X+Y
 
+        /* Calculate X and Y from X + Y and X - Y
+         X = ((X + Y) + (X - Y)) / 2
+         Y = X - (X - Y)*/
         long long x = (val1 + val2) / 2;
         long long y = x - val1;
 
+        // Return the results as {repeating, missing}
         return {(int)x, (int)y};
     }
 };
