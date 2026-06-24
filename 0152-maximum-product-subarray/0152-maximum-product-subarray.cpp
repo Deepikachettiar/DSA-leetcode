03a0dabd6093331e3623da367a69ffc0597ffef4
DEEPIKA K <deepika28226@gmail.com>
2026-06-24 19:55:01 +0530

    0 ms (100%)

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,7 +4,7 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int maxi=nums[0];
-        int curmax=nums[0];
+        int curmax=nums[0];    //the reason of having min also is due to the negative numbers 2 neg numbers product gives a positve an da bigger number. so swap when u see a neg number and multipy for max
         int curmin=nums[0];
 
         for(int i=1;i<nums.size();i++)
