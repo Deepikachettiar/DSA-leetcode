a07e39207e8f307830ba6233ca4ca389d11824ec
DEEPIKA K <deepika28226@gmail.com>
2026-06-24 20:15:44 +0530

    0 ms (100%)

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int start=0;
+        int end=n-1;
+
+        while(start<=end)
+        {
+            int index= floor((start+end)/2);
+            if(nums[index]==target)
+                return index;
+
+            else if(nums[index]>target)
+            {
+                end=index-1;
+                n=n-index;
+            }
+
+            else
+            {
+                start=index+1;
+                n=n-index;
+            }
+        }
+
+        return -1;
+    }
+};
