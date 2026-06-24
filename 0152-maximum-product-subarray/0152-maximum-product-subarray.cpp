d04a4b571abb6170d1104900867195b2cf26b0cd
DEEPIKA K <deepika28226@gmail.com>
2026-06-24 19:53:09 +0530

    0 ms (100%)

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxi=nums[0];
+        int curmax=nums[0];
+        int curmin=nums[0];
+
+        for(int i=1;i<nums.size();i++)
+        {
+            int x=nums[i];
+            if(x<0)
+            {
+                swap(curmax,curmin);
+            }
+
+            curmax=max(curmax*x,x);
+            curmin=min(curmin*x,x);
+            
+            if(curmax>maxi)
+                maxi=curmax;
+        }
+
+        return maxi;
+    }
+};
