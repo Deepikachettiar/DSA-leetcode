3781b4d1eb53592751ef28ed7f8a83a5254b4330
DEEPIKA K <deepika28226@gmail.com>
2026-07-30 20:38:32 +0530

    0 ms (100%)

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> arr;
+        int n=intervals.size();
+        int i=0;
+        while(i<n && intervals[i][1]<newInterval[0] )
+        {
+            arr.push_back(intervals[i]);
+            i++;
+        }
+
+        while(i<n && intervals[i][0] <= newInterval[1])
+        {
+            newInterval[0]=min(intervals[i][0],newInterval[0]);
+            newInterval[1]=max(intervals[i][1],newInterval[1]);
+            i++;
+        }
+
+        arr.push_back(newInterval);
+        while(i<n)
+        {
+            arr.push_back(intervals[i]);
+            i++;
+        }
+
+        return arr;
+    }
+};
