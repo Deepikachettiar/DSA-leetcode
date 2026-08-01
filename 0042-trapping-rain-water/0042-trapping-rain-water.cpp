83de006e0a1e5630c30fe8793772584371ad80c3
DEEPIKA K <deepika28226@gmail.com>
2026-08-01 17:35:07 +0530

    0 ms (100%)

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left=0;
+        int right=height.size()-1;
+        int lmax=0;
+        int rmax=0;
+        int total=0;
+
+        while(left<right)
+        {
+            if(height[left]<height[right])
+            {
+                if(height[left]>lmax)
+                {
+                    lmax=height[left];
+                }
+
+                else
+                {
+                    total+=lmax-height[left];
+                }
+
+                left++;
+            }
+
+            else
+            {
+                if(height[right]>rmax)
+                {
+                    rmax=height[right];
+                }
+
+                else
+                {
+                    total+=rmax-height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return total;
+    }
+};
