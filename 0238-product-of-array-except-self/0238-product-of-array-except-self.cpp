267a4fec613fe52f736793bb9e167dd1ebb446a5
DEEPIKA K <deepika28226@gmail.com>
2026-08-16 14:35:28 +0530

    3 ms (37.15%)

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int i=0;
+        vector<int> arr(nums.size(),1);
+        int left=1;
+        for(int i=0;i<nums.size();i++)
+        {
+            arr[i]=left;
+            left*=nums[i];
+        }
+
+        int right=1;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            arr[i]*=right;
+            right*=nums[i];
+        }
+
+        return arr;
+    }
+};
